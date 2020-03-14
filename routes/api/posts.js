e188bb1c273405c0e9ac878e3881ea0e9b21d98c
JaiRaga <raga.j.santhosh@gmail.com>
2020-03-14 20:38:02 +0530

    completed all the CRUD routes for post page

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -75,7 +75,7 @@ router.get("/:id", auth, async (req, res) => {
   }
 });
 
-// route  ==> DELETE /api/post/:id
+// route  ==> DELETE /api/posts/:id
 // desc   ==> Delete post by id
 // access ==> Private
 router.delete("/:id", auth, async (req, res) => {
@@ -97,6 +97,137 @@ router.delete("/:id", auth, async (req, res) => {
     if (err.kind === "ObjectId") {
       return res.status(404).send({ msg: "Post not found" });
     }
+    res.status(500).send("Server Error");
+  }
+});
+
+// route  ==> PATCH /api/posts/like/:id
+// desc   ==> Like post by id
+// access ==> Private
+router.patch("/like/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length > 0
+    ) {
+      return res.status(400).send({ msg: "Post already liked" });
+    }
+    post.likes.unshift({ user: req.user.id });
+    await post.save();
+    res.send(post.likes);
+  } catch (err) {
+    console.log(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).send({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
+// route  ==> PATCH /api/posts/unlike/:id
+// desc   ==> Like post by id
+// access ==> Private
+router.patch("/unlike/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length ===
+      0
+    ) {
+      return res.status(400).send({ msg: "Post has not yet been liked" });
+    }
+
+    const removeIndex = post.likes
+      .map(like => like.user.toString())
+      .indexOf(req.user.id);
+
+    post.likes.splice(removeIndex, 1);
+
+    await post.save();
+    res.send(post.likes);
+  } catch (err) {
+    console.log(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).send({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
+// route  ==> POST /api/post/comment/:id
+// desc   ==> Create a new comment
+// access ==> Private
+router.post(
+  "/comment/:id",
+  [
+    auth,
+    [
+      check("text", "Text is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send({ errors: errors.array() });
+    }
+
+    try {
+      const { avatar, name } = await User.findById(req.user.id);
+      const post = await Post.findById(req.params.id);
+      if (!post) {
+        return res.status(404).send({ msg: "Post not found" });
+      }
+
+      const comment = {
+        user: req.user.id,
+        text: req.body.text,
+        name,
+        avatar
+      };
+
+      post.comments.unshift(comment);
+      await post.save();
+
+      res.send(post.comments);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+// route  ==> DELETE /api/post/comment/:p_id/:comment_id
+// desc   ==> Delete a comment
+// access ==> Private
+router.delete("/comment/:p_id/:comment_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.p_id);
+    if (!post) {
+      return res.status(404).send({ msg: "Post not found" });
+    }
+
+    const comment = post.comments.find(
+      comment => comment.id === req.params.comment_id
+    );
+    if (!comment) {
+      return res.status(404).send({ msg: "Comment does not exists" });
+    }
+
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).send({ msg: "User not authorized" });
+    }
+
+    const removeIndex = post.comments
+      .map(comment => comment.id.toString())
+      .indexOf(req.params.comment_id);
+
+    post.comments.splice(removeIndex, 1);
+    await post.save();
+    res.send(post.comments);
+  } catch (err) {
+    console.log(err.message);
     res.status(500).send("Server Error");
   }
 });
