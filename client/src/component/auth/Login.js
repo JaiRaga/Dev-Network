dcea7c1858b438591ac8a42b64eee13729ca28f3
JaiRaga <raga.j.santhosh@gmail.com>
2020-03-15 20:55:13 +0530

    created sigup and login components

---
+++ client/src/component/auth/Login.js
@@ -0,0 +1,74 @@
+import React, { Fragment, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
+
+  const { email, password } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+
+    const newUser = {
+      email,
+      password
+    };
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+
+    const body = JSON.stringify(newUser);
+
+    const res = await axios.post("/api/auth", body, config);
+    console.log(res, "Data: **************", res.data);
+  };
+
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Sign into Your Account
+      </p>
+      <form className="form" action="create-profile.html" onSubmit={onSubmit}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={onChange}
+          />
+          <small className="form-text">
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={onChange}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign In</Link>
+      </p>
+    </Fragment>
+  );
+};
+
+export default Login;
