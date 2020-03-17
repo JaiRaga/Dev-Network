09c878acf130d27d2cc3aad9bfef749e0258d314
JaiRaga <raga.j.santhosh@gmail.com>
2020-03-17 21:35:36 +0530

    refactored alert actions

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 
 export const setAlert = (msg, alertType) => dispatch => {
   const id = uuidv4();
+
   dispatch({
     type: SET_ALERT,
     payload: { msg, alertType, id }
