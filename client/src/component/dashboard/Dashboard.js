ceb2a224d722816a63123013c12724e2cf54a12b
JaiRaga <raga.j.santhosh@gmail.com>
2020-03-26 21:13:22 +0530

    Completed dashboard page

--- client/src/component/dashboard/Dashboard.js
+++ client/src/component/dashboard/Dashboard.js
@@ -1,17 +1,41 @@
-import React, { useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import Spinner from "../layout/Spinner";
 import { getCurrentUserProfile } from "../../actions/profile";
+import DashboardActions from "./DashboardActions";
 
-const Dashboard = ({ getCurrentUserProfile, auth, profile }) => {
+const Dashboard = ({
+  getCurrentUserProfile,
+  auth: { user },
+  profile: { loading, profile }
+}) => {
   useEffect(() => {
     getCurrentUserProfile();
-  }, []);
+  }, [getCurrentUserProfile]);
 
-  return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+  return loading && profile === null ? (
+    Spinner
+  ) : (
+    <Fragment>
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        <i className="fas fa-user">Welcome {user && user.name}</i>
+      </p>
+      {profile !== null ? (
+        <Fragment>
+          <DashboardActions />
+        </Fragment>
+      ) : (
+        <Fragment>
+          <p>You have not setup a profile. Please add some info!</p>{" "}
+          <Link to="create-profile" className="btn btn-primary my-1">
+            Create Profile
+          </Link>
+        </Fragment>
+      )}
+    </Fragment>
   );
 };
 
