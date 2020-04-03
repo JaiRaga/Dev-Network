b6c57e6aed50f6ecc7b6fb93679e373f40795362
JaiRaga <raga.j.santhosh@gmail.com>
2020-04-03 20:41:50 +0530

    refactored getprofilebyid

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -47,7 +47,7 @@ export const getProfiles = () => async dispatch => {
 };
 
 // Get profiles by user id
-export const getProfilesById = userId => async dispatch => {
+export const getProfileById = userId => async dispatch => {
   try {
     const res = await axios.get(`/api/profile/user/${userId}`);
 
