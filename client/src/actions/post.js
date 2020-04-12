91e44a7b9c1e8eca3490a08cce34cc6f6ff02866
JaiRaga <raga.j.santhosh@gmail.com>
2020-04-12 20:35:45 +0530

    created actions for adding and removing likes and deleting posts

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { GET_POSTS, POST_ERROR } from "./types";
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from "./types";
 
+// Get all posts
 export const getPosts = () => async (dispatch) => {
   try {
     const res = await axios.get("/api/posts");
@@ -11,6 +12,60 @@ export const getPosts = () => async (dispatch) => {
       payload: res.data
     });
   } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Add Like
+export const addLike = (id) => async (dispatch) => {
+  try {
+    const res = await axios.patch(`/api/posts/like/${id}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: res.data }
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Remove Like
+export const removeLike = (id) => async (dispatch) => {
+  try {
+    const res = await axios.patch(`/api/posts/unlike/${id}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: res.data }
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Delete Post
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await axios.delete(`/api/posts/${id}`);
+
+    dispatch({
+      type: DELETE_POST,
+      payload: id
+    });
+
+    dispatch(setAlert("Post Removed", "success"));
+  } catch (err) {
+    console.log(err);
     dispatch({
       type: POST_ERROR,
       payload: { msg: err.response.statusText, status: err.response.status }
