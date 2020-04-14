97b7a6f69fe83972cea53c8e10845b892fc4ecdb
JaiRaga <raga.j.santhosh@gmail.com>
2020-04-14 18:59:23 +0530

    refactored server.js file

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const app = express();
 connectDB();
 
 // Init middleware
-app.use(express.json({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
